690158da4d6a71ce301f931d161219b084288a03
drew0821 <tshipp909@gmail.com>
2020-08-01 13:06:12 -0700

    added custom javascript for search bar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 
 baguetteBox.run('.photo-gallery');
-const search = new Filter('search', 'data-caption');
+
+function search() {
+    let input = document.getElementById('search').value.toLowerCase();
+    let images = document.querySelectorAll('a');
+    for (let i = 0; i < images.length; i ++) {
+        let link = images[i];
+        let caption = link.getAttribute('data-title').toLowerCase();
+        if (caption.indexOf(input) > -1) {
+            link.style.display = '';
+        } else {
+           link.style.display = 'none';
+        }
+    }
+}
